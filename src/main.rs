c4f3641ca23b3c66ca55f663f5e2fb078e6749a7
Josh Austin <83935880+joshaustintech@users.noreply.github.com>
2024-09-10 00:17:19 -0500

    switching out Poem for Rocket

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,81 @@
-use color_eyre::eyre::Result;
-use once_cell::sync::Lazy;
-use poem::{error::InternalServerError, get, handler, listener::TcpListener, web::{Html, Path}, Route, Server};
-use tera::{Tera, Context};
+#[macro_use] extern crate rocket;
 
-static TEMPLATES: Lazy<Tera> = Lazy::new(|| {
-    let mut tera = match Tera::new("templates/**/*") {
-        Ok(t) => t,
-        Err(e) => {
-            println!("Parsing error(s): {e}");
-            ::std::process::exit(1);
-        }
-    };
-    tera.autoescape_on(vec![".html", ".sql"]);
-    tera
-});
+#[cfg(test)] mod tests;
 
-#[handler]
-fn hello() -> Result<Html<String>, poem::Error> {
-    let mut context = Context::new();
-    TEMPLATES
-        .render("index.html", &context)
-        .map_err(InternalServerError)
-        .map(Html)
+#[derive(FromFormField)]
+enum Lang {
+    #[field(value = "en")]
+    English,
+    #[field(value = "ru")]
+    #[field(value = "ру")]
+    Russian
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    color_eyre::install()?;
-    let app = Route::new().at("/", get(hello));
-    Ok(
-        Server::new(TcpListener::bind("0.0.0.0:3000"))
-            .run(app)
-            .await?
-    )
+#[derive(FromForm)]
+struct Options<'r> {
+    emoji: bool,
+    name: Option<&'r str>,
+}
+
+// Try visiting:
+//   http://127.0.0.1:8000/hello/world
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+
+// Try visiting:
+//   http://127.0.0.1:8000/hello/мир
+#[get("/мир")]
+fn mir() -> &'static str {
+    "Привет, мир!"
+}
+
+// Try visiting:
+//   http://127.0.0.1:8000/wave/Rocketeer/100
+#[get("/<name>/<age>")]
+fn wave(name: &str, age: u8) -> String {
+    format!("👋 Hello, {} year old named {}!", age, name)
+}
+
+// Note: without the `..` in `opt..`, we'd need to pass `opt.emoji`, `opt.name`.
+//
+// Try visiting:
+//   http://127.0.0.1:8000/?emoji
+//   http://127.0.0.1:8000/?name=Rocketeer
+//   http://127.0.0.1:8000/?lang=ру
+//   http://127.0.0.1:8000/?lang=ру&emoji
+//   http://127.0.0.1:8000/?emoji&lang=en
+//   http://127.0.0.1:8000/?name=Rocketeer&lang=en
+//   http://127.0.0.1:8000/?emoji&name=Rocketeer
+//   http://127.0.0.1:8000/?name=Rocketeer&lang=en&emoji
+//   http://127.0.0.1:8000/?lang=ru&emoji&name=Rocketeer
+#[get("/?<lang>&<opt..>")]
+fn hello(lang: Option<Lang>, opt: Options<'_>) -> String {
+    let mut greeting = String::new();
+    if opt.emoji {
+        greeting.push_str("👋 ");
+    }
+
+    match lang {
+        Some(Lang::Russian) => greeting.push_str("Привет"),
+        Some(Lang::English) => greeting.push_str("Hello"),
+        None => greeting.push_str("Hi"),
+    }
+
+    if let Some(name) = opt.name {
+        greeting.push_str(", ");
+        greeting.push_str(name);
+    }
+
+    greeting.push('!');
+    greeting
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![hello])
+        .mount("/hello", routes![world, mir])
+        .mount("/wave", routes![wave])
 }
